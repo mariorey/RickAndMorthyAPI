b93d92d2e8b8af2af9581c535eb12c6a7c1c430c
mariorey <marioreymario@gmail.com>
2023-02-27 13:35:01 +0100

    Many more funcionalities

--- js/js.js
+++ js/js.js
@@ -1,72 +1,76 @@
 let page = 1;
 let isLoading = false;
-renderCharacters()
+let speciesSet = new Set()
+
+renderCharacters();
+
 async function renderCharacters(query = "", page = 1) {
     isLoading = true;
 
-    const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${query}&page=${page}`, {});
+    let statusFilter = document.querySelector('#statusFilter input[name="statusOption"]:checked')
+    if(statusFilter==null){statusFilter = ""}
+    else{statusFilter = statusFilter.value}
+
+    const urlParams = new URLSearchParams({
+        name: query,
+        page: page,
+        status: statusFilter,
+    });
+
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?${urlParams.toString()}`, {});
     const data = await response.json();
     let characters = data.results;
-    const main = document.querySelector("main");
+
+    const content = document.getElementById("content");
 
     if (page === 1) {
-        main.textContent = '';
+        content.textContent = '';
     }
 
     characters.forEach(character => {
-        const card__container = document.createElement('div');
-        const card__image = document.createElement('img');
-        const card__name = document.createElement('p');
-        const card__species = document.createElement('p');
-        const card__origin = document.createElement('origin');
-        card__container.classList.add('card__container');
-        card__image.classList.add('card__image');
-        card__name.classList.add('card__name');
-        card__species.classList.add('card__species');
-        card__origin.classList.add('card__origin');
 
-        card__image.src = character.image;
-        card__name.innerText = `Name: ${character.name}`;
-        card__species.innerText = `Species: ${character.species}`;
-        card__origin.innerText = `Origin: ${character.origin.name}`;
-        card__container.appendChild(card__image);
-        card__container.appendChild(card__name);
-        card__container.appendChild(card__species);
-        card__container.appendChild(card__origin);
+        const card = document.createRange().createContextualFragment(
+            `
+            <div class="card__container">
+            <img class="card__image" src=${character.image} alt="${character.name}" loading="lazy">
+            <h3 class="card__name">Name: ${character.name}</h3>
+            <h3 class="card__status, ${character.status}">${character.status}</h3>
+            <p class="card__species">Species: ${character.species}</p>
+            <p class="card__origin">Origin: ${character.origin.name}</p>
+            </div>
+                
+            `);
 
-        main.appendChild(card__container);
+        content.appendChild(card);
     });
 
     isLoading = false;
 }
 
-function debounceRenderCharacters(query) {
-    // Cancela el temporizador existente antes de iniciar uno nuevo
-    clearTimeout(debounceTimerId);
-
-    // Inicia un nuevo temporizador para llamar a renderCharacters después de 500ms
-    debounceTimerId = setTimeout(() => {
-        page = 1;
-        renderCharacters(query, page);
-    }, 500);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
     const search__term = document.getElementById('search__term');
     const debouncedRenderCharacters = debounce(renderCharacters, 500);
 
     search__term.addEventListener('input', () => {
         const query = search__term.value;
+        if(query.toString().includes("evil")){
+            const evilAudio = document.querySelector('#evil-morty');
+            setTimeout(() => {   evilAudio.play() }, 500);
+
+        }
         debouncedRenderCharacters(query);
     });
 
     window.addEventListener('scroll', () => {
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !isLoading) {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 100 && !isLoading) {
             page++;
             renderCharacters(search__term.value, page);
         }
     });
-});
+
+    document.querySelector('#statusFilter').addEventListener('change', () => {
+        renderCharacters(search__term.value, 1);
+    });
+
 
 function debounce(func, delay) {
     let timerId;
@@ -80,5 +84,22 @@ function debounce(func, delay) {
         }, delay);
     };
 }
+
+/*function loadFilters(characters, speciesSet = new Set) {
+    let species = document.getElementById("species")
+    characters.forEach(character => {
+        speciesSet.add(character.status)
+    })
+    speciesSet.forEach(specie => {
+        const check = document.createRange().createContextualFragment(`
+                 <div>
+                <input type="checkbox" id=${specie} name=${specie} value=${specie} />
+                <label for=${specie}>${specie}</label>
+                </div>`)
+
+        species.append(check)
+    return speciesSet})
+    }*/
+
 
 
